3334a45a79f1551787709dd34b1beaf5520e076a
mazeng123 <2716175579@qq.com>
2025-10-26 15:49:32 +0800

    新代码位置改变

---
+++ build/CMakeFiles/cpp_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_tutorial.dir/source/src/Student.cpp.obj"
+  "CMakeFiles/cpp_tutorial.dir/source/src/Student.cpp.obj.d"
+  "CMakeFiles/cpp_tutorial.dir/source/src/main.cpp.obj"
+  "CMakeFiles/cpp_tutorial.dir/source/src/main.cpp.obj.d"
+  "CMakeFiles/cpp_tutorial.dir/source/src/utils.cpp.obj"
+  "CMakeFiles/cpp_tutorial.dir/source/src/utils.cpp.obj.d"
+  "bin/cpp_tutorial.exe"
+  "bin/cpp_tutorial.exe.manifest"
+  "bin/cpp_tutorial.pdb"
+  "libcpp_tutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
