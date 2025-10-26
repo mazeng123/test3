cee744f0ad84b87c490ee05c1d135360509989cc
mazeng123 <2716175579@qq.com>
2025-10-26 16:03:10 +0800

    “整理”

---
+++ c++/.vscode/src/test2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	double height;
+	double t=3.0;
+	double g=10.0;
+	height=0.5*g*t*t; 
+	printf("height = %.2f",height); 
+	int k;
+	k=3.5;
+	printf("\n%d",k); 
+	return 0;
+} 
